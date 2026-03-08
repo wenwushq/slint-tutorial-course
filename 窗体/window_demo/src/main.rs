36cad65a68b8e1f7bb9e36be6cee2e6ca12c1a15
wenwushq <41138799+wenwushq@users.noreply.github.com>
2026-03-08 18:04:56 +0800

    Add files via upload

---
+++ 窗体/window_demo/src/main.rs
@@ -0,0 +1,16 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let main_window = MainWindow::new()?;
+
+    //main_window.window().set_maximized(true);
+    
+
+    main_window.run()?;
+    Ok(())
+}
